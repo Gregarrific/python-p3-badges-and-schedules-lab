051c80836771471506a5bca09530a14acd871a9a
Gregory Dutton <greg.dutton@gmail.com>
2023-10-10 09:32:41 -0500

    refactor lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,36 +1,19 @@
 def badge_maker(name):
-    msg = f"Hello, my name is {name}."
-    return msg
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    batch_msg = []
-    for name in names:
-        msg = f"Hello, my name is {name}."
-        batch_msg.append(msg)
-    return batch_msg
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
     rooms_msg = []
     room = 1
-    def assign_room(room, name):
-        room_msg = f"Hello, {name}! You'll be assigned to room {room}!"
-        return room_msg
     for name in names:
-        rooms_msg.append(assign_room(room, name))
+        rooms_msg.append(f"Hello, {name}! You'll be assigned to room {room}!")
         room += 1
     return rooms_msg
 
 def printer(names):
-    
-    def my_print(list):
-        for item in list:
-            print(item)
-    
-    print_list = []
-    print_list = batch_badge_creator(names)
-    my_print(print_list)
-    
-    print_list = assign_rooms(names)
-    my_print(print_list)
-    
-    return None
+    for name in batch_badge_creator(names):
+        print (name)
+    for room in assign_rooms(names):
+        print (room)
